72022e9896d0fb9058158a5546c6250687fe55c1
MartinRo <martin@rodemann.org>
2015-04-02 09:35:52 +0200

    added some missing repos also removed not longer needed ones and point bacon kernel to  new branch
    Change-Id: I6432254436218492b4dd5bc239475bde340bf3c9

--- kernel_special_cases.sh
+++ kernel_special_cases.sh
@@ -3,45 +3,45 @@
 
 ## all non-specified kernels are assumed to be from CyanogenMod
 export remote="cm"
-export remoterevision="cm-12.0"
+export remoterevision="cm-12.1"
 
 case $1 in
     bacon|find7*)
         export remote="team-exodus"
-        export remoterevision="EXODUS"
+        export remoterevision="staging/EXODUS-5.1"
         export kernelsource="kernel_oneplus_msm8974"
     ;;
-    d80*|ls980|vs980)
-        export remote="vanir"
-        export remoterevision="L5"
-        export kernelsource="kernel_lge_msm8974"
-    ;;
-    e980)
-        export kernelsource="lge-kernel-gproj"
-    ;;
-    hammerhead)
-        export remote="vanir"
-        export remoterevision="L5"
-        export kernelsource="kernel_lge_hammerhead"
-    ;;    
-    hammerheadcaf)
-        export remote="cm"
-        export remoterevision="cm-12.0-caf"
-        export kernelsource="android_kernel_lge_hammerhead"
-    ;;
-    flo|deb|mako)
-        export remote="github"
-        export remoterevision="L5"
-        export kernelsource="AlmightyMegadeth00/kernel_google_msm"
-    ;;
+#    d80*|ls980|vs980)
+#        export remote="vanir"
+#        export remoterevision="L5"
+#        export kernelsource="kernel_lge_msm8974"
+#    ;;
+#    e980)
+#        export kernelsource="lge-kernel-gproj"
+#    ;;
+#    hammerhead)
+#        export remote="vanir"
+#        export remoterevision="L5"
+#        export kernelsource="kernel_lge_hammerhead"
+#    ;;    
+#    hammerheadcaf)
+#        export remote="cm"
+#        export remoterevision="cm-12.0-caf"
+#        export kernelsource="android_kernel_lge_hammerhead"
+#    ;;
+#    flo|deb|mako)
+#        export remote="github"
+#        export remoterevision="L5"
+#        export kernelsource="AlmightyMegadeth00/kernel_google_msm"
+#    ;;
     flounder)
         export remote="github"
         export remoterevision="L5"
         export kernelsource="AlmightyMegadeth00/kernel_tegra"
     ;;
-    xt926|moto_msm8960)
-        export remote="ExInv"
-        export remoterevision="EXODUS"
-        export kernelsource="kernel_motorola_msm8960dt-common"
-    ;;
+#    xt926|moto_msm8960)
+#        export remote="ExInv"
+#        export remoterevision="EXODUS"
+#        export kernelsource="kernel_motorola_msm8960dt-common"
+#    ;;
 esac
